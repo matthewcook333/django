36a2cb1596b7cdd2691f58f74b05dc8aeb6b5ae5
matthewcook333 <matthewcook333@gmail.com>
2014-01-09 17:43:23 -0800

    editing urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
